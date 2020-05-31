cf98294e7124ea7a1d616bc26b31880c8b57d3c8
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 22:59:38 -0500

    Using continue to skip directly to the condition

--- For_Loops.cpp
+++ For_Loops.cpp
@@ -7,9 +7,23 @@ int main(){
 		cout<<"Time "<<i<<" saying Hello"<<endl;
 	
 		if(i==7){
-			break;
+			break;	//using BREAK to finish the loop for good
 		}
 	}
 	cin.get();
+
+
+
+
+	for(int i=1;i<=10;i++){
+
+                if(i==7){
+                        continue;  //using continue to skip to the condition
+                }
+		cout<<"Time "<<i<<" saying Hello"<<endl;
+        }
+        cin.get();
+
+
 	return 0;
 }
